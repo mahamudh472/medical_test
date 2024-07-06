cfc3766842389e351be9ba82913858936e12f16d
Asif Biswas <asifbiswas85@gmail.com>
2024-07-06 21:50:47 +0600

    feat: Improve request count logic in context processor

--- request/context_processors.py
+++ request/context_processors.py
@@ -1,12 +1,18 @@
 from .models import Request
 
 def request_count(request):
+    count = 0
     if request.user.is_authenticated:
-        count = Request.objects.filter(user=request.user).first().tests.count()
+        count = Request.objects.filter(user=request.user).first()
+        if count:
+            count = count.tests.count()
     else:
         key = request.session.session_key
         if not key:
             request.session.create()
             key = request.session.session_key
-        count = Request.objects.filter(anynomous_user=key).first().tests.count()
+        count = Request.objects.filter(anynomous_user=key).first()
+        if count:
+            count = count.tests.count()
+
     return {"request_count": count}
