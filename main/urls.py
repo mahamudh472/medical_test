9392221e48ae0f8787464e8ad663b25a4ab55538
Mahmud Hasan <mahadymahamudh472@gmail.com>
2024-08-03 13:33:09 +0600

    search functionality improved

--- main/urls.py
+++ main/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("all-tests/", views.all_tests, name="all_tests"),
     path("all-test-sets/", views.all_test_sets, name="all_test_sets"),
     path("test-sets/<int:id>/", views.test_sets, name="test_sets"),
-    path("search/", views.search, name="search"),
+    path("search/", views.search_service, name="search"),
     path("suggestions/", views.search_suggestions, name="search_suggestions"),
     path("insert_test_data/", views.insert_test_data, name="insert_test_data"),
 ]
