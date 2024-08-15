b272a3a5464c5854f1a6c45e9b98044427fbb42a
Mahmud Hasan <mahadymahamudh472@gmail.com>
2024-08-15 22:39:03 +0600

    voucher issue fixed

--- request/utils.py
+++ request/utils.py
@@ -4,7 +4,7 @@ import requests
 from django.conf import settings
 
 
-def check_and_redeem_voucher(voucher_token):
+def voucher_isvalid(voucher_token):
     api_url = 'http://cloudscript.business/api/voucher/'
     headers = {
         'Authorization': f'Token {settings.CLOUDSCRIPT_API_KEY}',  # Use "Token" prefix
@@ -13,22 +13,41 @@ def check_and_redeem_voucher(voucher_token):
     # Ensure the voucher_token is correctly inserted into the URL
     check_url = f'{api_url}/check/{voucher_token}/'
     check_response = requests.get(check_url, headers=headers)
-
     if check_response.status_code == 200:
         voucher_data = check_response.json()
         if voucher_data.get('valid'):
-            # Redeem the voucher (using POST method)
-            redeem_url = f'{api_url}/redeem/{voucher_token}/'
-            redeem_response = requests.post(redeem_url, headers=headers)
-
-            if redeem_response.status_code == 200:
-                return redeem_response.json()  # Successfully redeemed
-            else:
-                redeem_response.raise_for_status()
+            return voucher_data
         else:
-            return {'error': 'Invalid voucher token', 'voucher': voucher_data.get('valid')}
+            return {"error": "Token is not valid"}
     else:
         check_response.raise_for_status()
+
+
+# def check_and_redeem_voucher(voucher_token):
+#     api_url = 'http://cloudscript.business/api/voucher/'
+#     headers = {
+#         'Authorization': f'Token {settings.CLOUDSCRIPT_API_KEY}',  # Use "Token" prefix
+#     }
+#
+#     # Ensure the voucher_token is correctly inserted into the URL
+#     check_url = f'{api_url}/check/{voucher_token}/'
+#     check_response = requests.get(check_url, headers=headers)
+#
+#     if check_response.status_code == 200:
+#         voucher_data = check_response.json()
+#         if voucher_data.get('valid'):
+#             # Redeem the voucher (using POST method)
+#             redeem_url = f'{api_url}/redeem/{voucher_token}/'
+#             redeem_response = requests.post(redeem_url, headers=headers)
+#
+#             if redeem_response.status_code == 200:
+#                 return redeem_response.json()  # Successfully redeemed
+#             else:
+#                 redeem_response.raise_for_status()
+#         else:
+#             return {'error': 'Invalid voucher token', 'voucher': voucher_data.get('valid')}
+#     else:
+#         check_response.raise_for_status()
 
 
 def create_voucher_snippet(discount_amount):
